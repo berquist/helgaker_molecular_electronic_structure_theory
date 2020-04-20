2a50fffce6a35ebc4ec2bd198e4ee028aadc65f1
Eric Berquist <eric.berquist@gmail.com>
2020-04-19 21:55:17 -0400

    Initial stab at creation and annihilation operators

--- ch1.nim
+++ ch1.nim
@@ -20,6 +20,41 @@ proc `*`(left, right: ONVector): ON =
                                      int(t[0] == t[1]).numToON())
   foldl(delta, a * b, int(1))
 
+type
+  ## Was the result of applying an operator successful (producing a new
+  ## operator number vector), or did it fail (produce zero)?
+  OperatorKind = enum
+    opkindSuccess,
+    opkindFailure
+  OperatorResult = object
+    case kind: OperatorKind
+    of opkindSuccess:
+      res: ONVector
+    of opkindFailure:
+      failure: int # TODO enforce zero
+
+## Apply the creation operator at the given index.
+proc create(v: ONVector, index: int): OperatorResult =
+  case v[index]:
+    of 0:
+      # TODO needs copy?
+      var r = v
+      r[index] = 1
+      return OperatorResult(kind: opkindSuccess, res: r)
+    of 1:
+      return OperatorResult(kind: opkindFailure)
+
+## Apply the annihilation operator at the given index.
+proc annihilate(v: ONVector, index: int): OperatorResult =
+  case v[index]:
+    of 0:
+      return OperatorResult(kind: opkindFailure)
+    of 1:
+      # TODO needs copy?
+      var r = v
+      r[index] = 0
+      return OperatorResult(kind: opkindSuccess, res: r)
+
 when isMainModule:
   let
     vac = @[0, 0, 0, 0].toONVector()
@@ -30,3 +65,7 @@ when isMainModule:
   doAssert in1 * in1 == 1
   doAssert in1 * in2 == 0
   doAssert in1 * in12 == 0
+  echo vac.create(0)
+  echo vac.annihilate(0)
+  echo in12.create(1)
+  echo in12.annihilate(1)
